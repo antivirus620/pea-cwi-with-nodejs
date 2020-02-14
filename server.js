35106e1de6326cbdb0256eafce9544ba94fb1cdb
arigato <teeruch.k@gmail.com>
2020-02-14 21:22:02 +0700

    Express Router

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const morgan = require('morgan'); // logger console
 const colors = require('colors');
 
+// import routes files
+const lines = require('./routes/lines');
+
 // Load ENV
 dotenv.config({ path: './config/config.env' });
 
@@ -15,40 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Routes
-// @desc    Get all organization line
-// @route   GET /api/v1/lines
-// @access  Public
-app.get('/api/v1/lines', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all lines' });
-});
-
-// @desc    Get single organization line
-// @route   GET /api/v1/lines/:id
-// @access  Public
-app.get('/api/v1/lines/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show single line' });
-});
-
-// @desc    Create organization line
-// @route   POST /api/v1/lines/
-// @access  Private
-app.post('/api/v1/lines/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create organization line' });
-});
-
-// @desc    Update organization line
-// @route   PUT /api/v1/lines/:id
-// @access  Private
-app.put('/api/v1/lines/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Update organization line' });
-});
-
-// @desc    Update organization line
-// @route   DELETE /api/v1/lines/:id
-// @access  Private
-app.delete('/api/v1/lines/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Delete organization line' });
-});
+app.use('/api/v1/lines', lines);
 
 const PORT = process.env.PORT || 5000;
 
