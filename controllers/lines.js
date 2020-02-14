0554f03d482d7de6283bd434753866c84c9d6a26
arigato <teeruch.k@gmail.com>
2020-02-14 22:00:48 +0700

    Create Controllers methods

---
+++ controllers/lines.js
@@ -0,0 +1,39 @@
+// @desc    Get all organization line
+// @route   GET /api/v1/lines
+// @access  Public
+// * export function
+exports.getLines = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show all lines' });
+};
+
+// @desc    Get single organization line
+// @route   GET /api/v1/lines/:id
+// @access  Public
+exports.getLine = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show single line ${req.params.id}` });
+};
+
+// @desc    Create organization line
+// @route   POST /api/v1/lines/
+// @access  Private
+exports.createLine = (req, res, next) => {
+  res.status(201).json({ success: true, msg: 'Create organization line' });
+};
+
+// @desc    Update organization line
+// @route   PUT /api/v1/lines/:id
+// @access  Private
+exports.updateLine = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update organization line ${req.params.id}` });
+};
+
+// @desc    Update organization line
+// @route   DELETE /api/v1/lines/:id
+// @access  Private
+exports.deleteLine = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Delete organization line' });
+};
--- routes/lines.js
+++ routes/lines.js
@@ -1,43 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-// @desc    Get all organization line
-// @route   GET /api/v1/lines
-// @access  Public
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all lines' });
-});
+// Import from controllers
+const {
+  getLines,
+  getLine,
+  createLine,
+  updateLine,
+  deleteLine
+} = require('../controllers/lines');
 
-// @desc    Get single organization line
-// @route   GET /api/v1/lines/:id
-// @access  Public
-router.get('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show single line ${req.params.id}` });
-});
+router
+  .route('/')
+  .get(getLines)
+  .post(createLine);
 
-// @desc    Create organization line
-// @route   POST /api/v1/lines/
-// @access  Private
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create organization line' });
-});
-
-// @desc    Update organization line
-// @route   PUT /api/v1/lines/:id
-// @access  Private
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update organization line ${req.params.id}` });
-});
-
-// @desc    Update organization line
-// @route   DELETE /api/v1/lines/:id
-// @access  Private
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Delete organization line' });
-});
+router
+  .route('/:id')
+  .get(getLine)
+  .put(updateLine)
+  .delete(deleteLine);
 
 module.exports = router;
