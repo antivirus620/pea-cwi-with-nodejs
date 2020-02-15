47295816bbcea297648a443fa60e15935b7032ef
arigato <teeruch.k@gmail.com>
2020-02-15 14:57:57 +0700

    Database Seeder from organization line

---
+++ seeder.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env
+dotenv.config({ path: './config/config.env' });
+
+// Load Models
+const Line = require('./models/Line');
+
+// ConnectDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read JSON Files
+const lines = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/organizationLine.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Line.create(lines);
+
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err.red);
+  }
+};
+
+// Delete data from DB
+const deleteData = async () => {
+  try {
+    await Line.deleteMany();
+
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err.red);
+  }
+};
+
+// รับคำสั่งจาก console
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
